5a1c2c6b97283fe83fd6e74a876634f4ead25ecc
voltakits <pertamake60gmail.com>
2026-09-22 12:19:07 +0700

    first commit wleeee

---
+++ latihan-pertama.dart
@@ -0,0 +1,15 @@
+void main() {
+  for (var i = 1; i < 10; i++) {
+    print('hello ${i + 1}');
+  }
+  print('wkwkwk');
+  
+  
+ 
+  
+    String? dayinmylife = "A Day In My Life Azril And Cahyo";
+    print(dayinmylife);
+
+   dayinmylife = null;
+   print(dayinmylife);
+}
