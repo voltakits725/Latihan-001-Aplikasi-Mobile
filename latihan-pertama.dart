a51ca9af32c8576bcee79c6623abbcc9ff7ad5b3
voltakits <pertamake60gmail.com>
2026-09-22 16:39:04 +0700

    2029 ganti presiden

--- latihan-pertama.dart
+++ latihan-pertama.dart
@@ -1,55 +1,13 @@
 void main() {
-  
-    String? dayinmylife = "A Day In My Life Azril And Cahyo";
-    print(dayinmylife.toUpperCase());
-
-   dayinmylife = null;
-   print(dayinmylife);
-
-   String namaProduk = 'Botol Plastik';
-
-   int umur = 20;
-    int jumlahBarang = 5;
-    int poin = 1000;
-
-    print('$umur $jumlahBarang $poin');
-    // bilangan double untuk pecahan angka desimal
-    double berat = 2.5;
-    double harga = 15000.50;
-    double rating = 4.8;
     
-    // num bisa menyimpan angka bilangan bulat maupun desimal
-    num nilai = 10;
-    nilai = 10.5;
+    String NIM = '1124160165';
+    String Nama = 'Rafly Kurniawan';
+    String Prodi = 'Teknik Informatika';
+    String Konsentrasi = 'Software Engineering';
 
-    // List digunakan untuk meyimpan data dengan tipe sama
-    List<String> namaMahasiswa = [
-    'Andi',
-    'Budi',
-    'Citra',
-    ];
+    bool sudahLulus = false;
+    
 
-    print(namaMahasiswa[0]);
-    print(namaMahasiswa[1]);
-    print(namaMahasiswa[2]);
 
-    //Set mirip List, tetapi tidak menyimpan nilai duplikat.
-
-    Set<String> kategori = {
-    'Plastik',
-    'Kertas',
-    'Plastik',
-    };
-    print(kategori);
-
-    Map<String, dynamic> mahasiswa = {
-    'nama': 'Budi',
-    'umur': 20,
-    'aktif': true,
-    };
-    // mengambil data
-    print(mahasiswa['nama']);
-    print(mahasiswa['umur']);
-    print(mahasiswa['aktif']);
 
 }
